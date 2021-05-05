ce7f9a598e56b1dc8b69a4683d176af91ad2795d
Jeniferignatius <jeni.igni@gmail.com>
2021-05-05 13:15:57 +0300

    form files added

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
   </React.StrictMode>,
   document.getElementById('root')
 );
+//var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+//ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
